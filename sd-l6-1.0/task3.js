25dbbeb9cfc40cd721d424e69451de4875f4ad4f
unknown <camiij.cjn@gmail.com>
2024-08-30 16:53:21 -0500

    actualizacion de codigo

--- sd-l6-1.0/task3.js
+++ sd-l6-1.0/task3.js
@@ -3,10 +3,10 @@
 
 import { getServerURL } from "./task1.js";
 
-export async function addUser(user) {
+export async function addUser(first_name, last_name, email) {
     
   const url = getServerURL();
-  const response = await fetch(url + "/users");
+  const response = await fetch("http://localhost:3000/users");
   const users = await response.json();
 
   // Generar un id de usuario
@@ -14,27 +14,27 @@ export async function addUser(user) {
 
   // Se crea un nuevo objeto usuario
   const newUser = {
-    id: newUderId.toString(),
-    first_name: firstName,
-    last_name: lastName,
+    id: newUderId,
+    first_name: first_name,
+    last_name: last_name,
     email: email
   };
 
   // Se envia un POST al servidor para crear un nuevo usuario
-  const addUserResponse = await fetch(url + "/users", {
+  const addUserResponse = await fetch("http://localhost:3000/users", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(newUser)
+    body: JSON.stringify({newUser})
   });
 
   const addUser = await addUserResponse.json();
   const output = `{
-  id: ${Number(addUser.id)},
+  id: ${addUser.id},
   first_name: '${addUser.first_name}',
   last_name: '${addUser.last_name}',
   email: '${addUser.email}'
 }`;
 
   // imprimir si la adicion del nuevo usuario fue exitoso
-  console.log(output)
+  return console.log(output)
 }
